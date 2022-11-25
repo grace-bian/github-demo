affdb8abc87df052c77650c3f79b35a5d6f984c1
Grace Bian <grace.bian@outlook.com>
2022-11-25 15:02:30 +1100

    delete

--- src/js/_lazi-size-bg.js
+++
@@ -1,9 +0,0 @@
-(function($){
-	document.addEventListener('lazybeforeunveil', function(e){
-		var bg = e.target.getAttribute('data-bg');
-		if(bg){
-			e.target.style.backgroundImage = 'url(' + bg + ')';
-		}
-	});
-})(jQuery);
-
